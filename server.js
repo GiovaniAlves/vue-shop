92031c9ffe461713c6f78e073f141e04922b364f
giovani.alves <giovani.alves.glv@gmail.com>
2022-05-02 15:27:45 -0300

    Atualização no build de produção

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const serveStatic = require('serve-static')
 const app = express()
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
+app.get(/.*/, function (req, res) {
+   res.sendFile(path.join(__dirname, 'dist/index.html'))
+})
+
 const port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started: ' + port)
