39da3520ea40121b92704128ad4739ec74b06052
giovani.alves <giovani.alves.glv@gmail.com>
2022-05-02 14:45:30 -0300

    Atualização no arquivo server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const path = require('path')
-const connectHistory = require('connect-history-api-fallback')
+const serveStatic = require('serve-static')
 
 const app = express()
-app.use(connectHistory(path.join(__dirname, 'dist')))
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
 const port = process.env.PORT || 5000
 app.listen(port)
