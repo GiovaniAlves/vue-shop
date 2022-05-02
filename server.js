9d74d4cd8f9838aff0fa409e3f01222e7d1aa840
giovani.alves <giovani.alves.glv@gmail.com>
2022-05-02 10:55:06 -0300

    Atualização no arquivo server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(serveStatic(path.join(__dirname, 'dist')))
 
 app.route('/*')
     .get(function (res) {
-          res.sendFile(path.join(__dirname, '/dist/index.html'))
+          res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
 
 const port = process.env.PORT || 5000
